3b23ca413dc1355cdcbbd8fcf0b1ab5f10854ffc
Leonardo Nogueira da Silva <leonardo.lns@outlook.com>
2020-04-20 10:57:13 -0300

    front end e style criados

---
+++ app/javascript/screens/discovery/index.js
@@ -0,0 +1,17 @@
+import React, { Fragment } from 'react';
+import SectionWrapper from '../../components/common/section_wrapper'
+import NavbarFooter from '../../components/common/navbar_footer';
+import Discovery  from '../../components/discovery';
+
+const DiscoveryScreen = () => {
+  return (
+    <Fragment>
+      <SectionWrapper>
+        <Discovery/>
+      </SectionWrapper>
+      <NavbarFooter />
+    </Fragment>
+
+  );
+}
+export default DiscoveryScreen;
